139f1c23dc2eee146a9a29106ec5857a039768fa
Ilya <qwerty9087650850@gmail.com>
2025-10-17 16:12:32 +0300

    chore: rm layers

--- mock-cbr-service/cmd/cbr/main.go
+++ mock-cbr-service/cmd/cbr/main.go
@@ -1,39 +1,5 @@
 package main
 
-import (
-	"context"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"mock-cbr-service/internal/app"
-)
-
 func main() {
-	r := app.New()
 
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
-
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %v", err)
-		}
-	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatalf("Server forced to shutdown: %v", err)
-	}
-	log.Println("Server exiting")
 }
